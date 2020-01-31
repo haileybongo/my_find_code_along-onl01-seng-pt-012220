46004d7890a6faac4764ea46f714fc304e912148
Hailey Bongo <haileyebongo@gmail.com>
2020-01-31 06:14:52 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -4,7 +4,8 @@ def my_find(collection)
   i = 0 
   while i < collection.length 
     if yield(collection[i])
-      return collection [1i]
+      return collection [i]
+    end
     i += 1 
   end
 
